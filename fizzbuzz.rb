d7b439a7d65f8c4010bd533a033234753e452484
Kyle Houghton <kylebenz@hotmail.com>
2018-12-18 23:49:33 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,14 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz
+def fizzbuzz(input_number)
+  if input_number % 15 == 0
+    return "FizzBuzz"
+  elsif input_number % 5 == 0
+    return "Buzz"
+  elsif input_number % 3 == 0
+    return "Fizz"
+  else
+    return nil
+  end
 end
